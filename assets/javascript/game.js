be906e967ffc5b5c4a693034094f2a90b720e82e
Ricky Sanchez <RickySanchez@rickys-imac.attlocal.net>
2018-06-25 16:43:34 -0500

    Added Opponent selection and enemy display css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -122,10 +122,95 @@ $(".character").on("click", function() {
         if (RickAndMortyRPG.characterSelected == "RickSanchez") {
             RickAndMortyRPG.characterObject = RickSanchez;
             $("#character_display").append(RickSanchezDiv);
-            $("#RickSanchez").html('<p>Rick Sanchez</p> <img src="assets/images/rick.png" border-radius="none" border="none"> <p id="RickSanchezHP">200 HP</p>');
+            $("#RickSanchez").html('<p>Rick</p> <img src="assets/images/rick.png" height="275"> <p id="RickSanchezHP">200 HP</p>');
         }
 
+        if (RickAndMortyRPG.characterSelected == "MortySmith") {
+            RickAndMortyRPG.characterObject = MortySmith;
+            $("#character_display").append(MortySmithDiv);
+            $("#MortySmith").html('<p>Morty</p> <img src="assets/images/mortySmith.png" height="215"> <p id="MortySmithHP">160 HP</p>');
+        }
 
+        if (RickAndMortyRPG.characterSelected == "SummerSmith") {
+            RickAndMortyRPG.characterObject = SummerSmith;
+            $("#character_display").append(SummerSmithDiv);
+            $("#SummerSmith").html('<p>Summer</p> <img src="assets/images/summerSmith.png" height="250"> <p id="SummerSmithHP">130 HP</p>');
+        }
+
+        if (RickAndMortyRPG.characterSelected == "BethSmith") {
+            RickAndMortyRPG.characterObject = BethSmith;
+            $("#character_display").append(BethSmithDiv);
+            $("#BethSmith").html('<p>Beth</p> <img src="assets/images/bethSmith.png" height="260"> <p id="BethSmithHP">145 HP</p>');
+        }
+
+        if (RickAndMortyRPG.characterSelected == "JerrySmith") {
+            RickAndMortyRPG.characterObject = JerrySmith;
+            $("#character_display").append(JerrySmithDiv);
+            $("#JerrySmith").html('<p>Jerry</p> <img src="assets/images/jerrySmith.png" height="275"> <p id="JerrySmithHP">100 HP</p>');
+        }
+
+        if (RickAndMortyRPG.characterSelected == "MrPoopyButthole") {
+            RickAndMortyRPG.characterObject = MrPoopyButthole;
+            $("#character_display").append(MrPoopyButtholeDiv);
+            $("#MrPoopyButthole").html('<p>Mr Poopy</p> <img src="assets/images/pencilRick.png" height="150"> <p id="MrPoopyButtholeHP">115 HP</p>');
+        }
+
+/*
+=======================================================
+Opponent Selection on.cllick task assignments 
+=======================================================
+*/
+        RickAndMortyRPG.gameStage = "opponents"
+
+    } else if (RickAndMortyRPG.gameStage == "opponents") {
+        RickAndMortyRPG.opponentSelected = $(this).attr("id");
+        $("#" + RickAndMortyRPG.opponentSelected).remove();
+        $("#prompts").text("The world is full of idiots who don't understand what's important, and they'll tear us apart");
+
+        console.log(RickAndMortyRPG.opponentSelected);
+
+        if (RickAndMortyRPG.opponentSelected == "RickSanchez") {
+            RickAndMortyRPG.opponentObject = RickSanchez;
+            $("#enemy_display").append(RickSanchezDiv);
+            $("#RickSanchez").html('<p>Rick</p> <img src="assets/images/rick.png" height="275"> <p id="RickSanchezHP">200 HP</p>');
+        }
+
+        if (RickAndMortyRPG.opponentSelected == "MortySmith") {
+            RickAndMortyRPG.opponentObject = MortySmith;
+            $("#enemy_display").append(MortySmithDiv);
+            $("#MortySmith").html('<p>Morty</p> <img src="assets/images/mortySmith.png" height="215"> <p id="MortySmithHP">160 HP</p>');
+        }
+
+        if (RickAndMortyRPG.opponentSelected == "SummerSmith") {
+            RickAndMortyRPG.opponentObject = SummerSmith;
+            $("#enemy_display").append(SummerSmithDiv);
+            $("#SummerSmith").html('<p>Summer</p> <img src="assets/images/summerSmith.png" height="250"> <p id="SummerSmithHP">130 HP</p>');
+        }
+
+        if (RickAndMortyRPG.opponentSelected == "BethSmith") {
+            RickAndMortyRPG.opponentObject = BethSmith;
+            $("#enemy_display").append(BethSmithDiv);
+            $("#BethSmith").html('<p>Beth</p> <img src="assets/images/bethSmith.png" height="260"> <p id="BethSmithHP">145 HP</p>');
+        }
+
+        if (RickAndMortyRPG.opponentSelected == "JerrySmith") {
+            RickAndMortyRPG.opponentObject = JerrySmith;
+            $("#enemy_display").append(JerrySmithDiv);
+            $("#JerrySmith").html('<p>Jerry</p> <img src="assets/images/jerrySmith.png" height="275"> <p id="JerrySmithHP">100 HP</p>');
+        }
+
+        if (RickAndMortyRPG.opponentSelected == "MrPoopyButthole") {
+            RickAndMortyRPG.opponentObject = MrPoopyButthole;
+            $("#enemy_display").append(MrPoopyButtholeDiv);
+            $("#MrPoopyButthole").html('<p>Mr Poopy</p> <img src="assets/images/pencilRick.png" height="150"> <p id="MrPoopyButtholeHP">115 HP</p>');
+        }
+
+/*
+=======================================================
+Fight Stage
+=======================================================
+*/
+        RickAndMortyRPG.gameStage = "Fight";
     }
 
  
